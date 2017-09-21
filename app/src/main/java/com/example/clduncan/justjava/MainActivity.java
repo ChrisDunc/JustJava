f8d472cc8344c23a16533ede9155f0b18c5c3bb7
ChrisDunc <christopher.duncan@stu.fayette.kyschools.us>
2017-09-21 10:10:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/clduncan/justjava/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.clduncan.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity--;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        String priceMessage = "Total: $" + (quantity *5) + "\nThank You!";
+        displayMessage(priceMessage);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price value on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
